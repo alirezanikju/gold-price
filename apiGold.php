698b8c9c82068e1a0b87570e413568f001f4c035
alireza nikju <alirezanikjoo2018@gmail.com>
2025-07-05 13:38:32 +0330

    creat api.php codes for gold total price

--- apiGold.php
+++ apiGold.php
@@ -1,1 +1,39 @@
 <?php
+
+header("Content-Type : application/json");
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+$rawData = file_get_contents("php://input");
+
+
+$data = json_decode($rawData , true);
+
+if(isset($data["amount"]) && is_numeric($data['amount'])){
+
+$price = 2500000;
+$amount = $data['amount'];
+
+$total = $price * $amount;
+
+echo json_encode([
+    "status" => "ok",
+    "price" => $price,
+    "amount" => $amount,
+    "totalPrice" => $total
+], JSON_UNESCAPED_UNICODE);
+
+}else{
+    echo json_encode([
+        "status" => "error",
+        "msg" => "اون مقدار گرم لعنتی رو درست وارد کن"
+    ], JSON_UNESCAPED_UNICODE);
+}
+
+
+}else{
+    echo json_encode([
+        "status"=> "error",
+        "msg"=> "فقط با post کار میکنیم"
+    ], JSON_UNESCAPED_UNICODE);
+}
+
