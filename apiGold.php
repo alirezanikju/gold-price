f41e497c9c3a7867211eeafd439df34d10563ec1
alireza nikju <alirezanikjoo2018@gmail.com>
2025-07-05 14:10:24 +0330

    creat curlGold.php for get data of api

--- apiGold.php
+++ apiGold.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Content-Type : application/json");
+header("Content-Type: application/json");
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 
 $rawData = file_get_contents("php://input");
